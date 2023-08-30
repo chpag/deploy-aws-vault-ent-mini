a4571e5acc729fb86ca9e2c013cdb97d5ede267a
chpag <115650339+chpag@users.noreply.github.com>
2023-08-30 15:14:35 +0200

    Create seecurity_group.tf

---
+++ seecurity_group.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "vault" {
+  name   = "${var.prefix}-vault"
+  vpc_id = module.vpc.vpc_id
+
+  tags = merge(
+    { Name = "${var.prefix}-vault-sg" },
+    var.common_tags,
+  )
+}
+
+resource "aws_security_group_rule" "vault_internal_api" {
+  description       = "Allow Vault nodes to reach other on port 8200 for API"
+  security_group_id = aws_security_group.vault.id
+  type              = "ingress"
+  from_port         = 8200
+  to_port           = 8200
+  protocol          = "tcp"
+  self              = true
+}
+
+resource "aws_security_group_rule" "vault_internal_raft" {
+  description       = "Allow Vault nodes to communicate on port 8201 for replication traffic, request forwarding, and Raft gossip"
+  security_group_id = aws_security_group.vault.id
+  type              = "ingress"
+  from_port         = 8201
+  to_port           = 8201
+  protocol          = "tcp"
+  self              = true
+}
