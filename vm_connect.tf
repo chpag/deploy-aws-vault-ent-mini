e98918d2e51285a6d12067e6209e9d5183afe00a
chpag <115650339+chpag@users.noreply.github.com>
2023-08-30 15:00:06 +0200

    Update vm_connect.tf

--- vm_connect.tf
+++ vm_connect.tf
@@ -28,7 +28,7 @@ data "aws_subnets" "public-subnet" {
 resource "aws_instance" "vault_cli_vm" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  subnet_id     = data.aws_subnets.public-asubnet.ids[0]
+  subnet_id     = data.aws_subnets.public-subnet.ids[0]
   key_name      = var.key_name
 
   vpc_security_group_ids = [aws_security_group.vault_sg.id]
