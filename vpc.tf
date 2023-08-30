97ffb9b48f906d1bbee5a9d5b98c3eb00f0e014e
chpag <115650339+chpag@users.noreply.github.com>
2023-08-30 15:08:32 +0200

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,13 @@
+
 module "vpc" {
   source                 = "terraform-aws-modules/vpc/aws"
   name                   = "${var.prefix}-vault"
   cidr                   = var.vpc_cidr
-  azs                    = var.azs
+  azs                    = [ var.az ]
   enable_nat_gateway     = true
   one_nat_gateway_per_az = true
-  private_subnets        = var.private_subnet_cidrs
-  public_subnets         = var.public_subnet_cidrs
+  private_subnets        = [ var.private_subnet_cidr ]
+  public_subnets         = [ var.public_subnet_cidr ]
 
   tags = var.vpc_tags
 }
