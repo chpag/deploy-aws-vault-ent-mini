b94985aa148192819bff1d1cfc398a54f9433087
chpag <115650339+chpag@users.noreply.github.com>
2023-08-30 15:18:34 +0200

    Update security_group.tf

--- security_group.tf
+++ security_group.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "vault_sg" {
 
 resource "aws_security_group_rule" "vault_internal_api" {
   description       = "Allow Vault nodes to reach other on port 8200 for API"
-  security_group_id = aws_security_group.vault.id
+  security_group_id = aws_security_group.vault_sg.id
   type              = "ingress"
   from_port         = 8200
   to_port           = 8200
@@ -20,10 +20,31 @@ resource "aws_security_group_rule" "vault_internal_api" {
 
 resource "aws_security_group_rule" "vault_internal_raft" {
   description       = "Allow Vault nodes to communicate on port 8201 for replication traffic, request forwarding, and Raft gossip"
-  security_group_id = aws_security_group.vault.id
+  security_group_id = aws_security_group.vault_sg.id
   type              = "ingress"
   from_port         = 8201
   to_port           = 8201
   protocol          = "tcp"
   self              = true
+}
+
+resource "aws_security_group_rule" "vault_ssh_inbound" {
+  count             = var.allowed_inbound_cidrs_ssh != null ? 1 : 0
+  description       = "Allow specified CIDRs SSH access to Vault nodes"
+  security_group_id = aws_security_group.vault.id
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = var.allowed_inbound_cidrs_ssh
+}
+
+resource "aws_security_group_rule" "vault_outbound" {
+  description       = "Allow Vault nodes to send outbound traffic"
+  security_group_id = aws_security_group.vault.id
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
 }
