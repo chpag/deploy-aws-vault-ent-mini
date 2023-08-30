8344d36ca415921c5dea46fe1ac51b877fbfe2cb
chpag <115650339+chpag@users.noreply.github.com>
2023-08-30 14:55:47 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -35,6 +35,32 @@ variable "allowed_inbound_cidrs_ssh" {
   default = null
 }
 
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default = [
+    "10.0.0.0/19",
+    "10.0.32.0/19",
+    "10.0.64.0/19",
+  ]
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default = [
+    "10.0.128.0/20",
+    "10.0.144.0/20",
+    "10.0.160.0/20",
+  ]
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
 variable "allowed_inbound_cidrs_lb" {
   description = "List of CIDR blocks to permit inbound traffic from to load balancer"
   default = null
