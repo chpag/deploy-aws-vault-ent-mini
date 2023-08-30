ba77bdc3b9791263dec264f294fa6b619373e84f
chpag <115650339+chpag@users.noreply.github.com>
2023-08-30 15:30:08 +0200

    Update vm_vault.tf

--- vm_vault.tf
+++ vm_vault.tf
@@ -25,6 +25,21 @@ data "aws_subnets" "public-subnet" {
   }
 }
 
+locals {
+  vault_user_data = templatefile(
+    var.user_supplied_userdata_path != null ? var.user_supplied_userdata_path : "${path.module}/templates/install_vault.sh.tpl",
+    {
+      region                  = var.aws_region
+      name                    = var.resource_name_prefix
+      vault_version           = var.vault_version
+      kms_key_arn             = var.kms_key_arn
+      s3_bucket_vault_license = var.aws_bucket_vault_license
+      vault_license_name      = var.vault_license_name
+      secrets_manager_arn     = var.secrets_manager_arn
+      leader_tls_servername   = var.leader_tls_servername
+    }
+  )
+}
 resource "aws_instance" "vault-ent_vm" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
