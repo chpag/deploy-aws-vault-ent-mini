7937754821eb2cc88062ddfba6e04876bcb3d1b0
chpag <115650339+chpag@users.noreply.github.com>
2023-08-30 14:44:29 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  # your AWS region
+  region = var.region
+}
